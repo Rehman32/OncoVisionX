68290c5e16997d6151e0ee2e1d3d8c49b65abe1e
Naeem <135511325+Rehman32@users.noreply.github.com>
2025-11-22 23:32:24 +0500

    implemented login logic

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -1,7 +1,8 @@
 import { Request, Response, NextFunction } from "express";
-import { BadRequestError, ConflictError } from "../utils/errors";
+import { BadRequestError, ConflictError, UnauthorizedError } from "../utils/errors";
 import User, { IUser } from "../models/User";
 import { generateAccessToken, generateRefreshToken } from "../utils/jwt";
+import { userInfo } from "os";
 
 export const register = async (
   req: Request,
@@ -72,3 +73,67 @@ export const register = async (
     next(error);
   }
 };
+
+
+//login user 
+
+export const login = async (req:Request,res:Response,next:NextFunction) : Promise<void> => {
+  try {
+    const {email, password} = req.body;
+
+
+    if(!email || !password){
+      throw new BadRequestError("Email and password are required .")
+    }
+
+    const user =await User.findOne({email:email.toLowerCase()}).select('+password');
+
+    if(!user){
+      throw new UnauthorizedError("Invalid email or password")
+    }
+
+    if(!user.isActive){
+      throw new BadRequestError("Your Account has been  deactivated , Please contact support. ")
+    }
+
+    const isPasswordValid = await user.comparePassword(password);
+
+    if(!isPasswordValid){
+      throw new UnauthorizedError("Password is Invalid. ")
+    }
+
+    user.lastLogin=new Date();
+
+    await user.save();
+
+    const accessToken = generateAccessToken(
+      {
+        userId : user._id.toString(),
+        email: user.email,
+        role: user.role
+      }
+    );
+
+    const refreshToken = generateRefreshToken({
+      userId:user._id.toString(),
+      email : user.email,
+      role : user.role
+    });
+
+    const userResponse =user.toJSON();
+
+    res.status(200).json({
+      success : true,
+      message : 'Login Successful',
+      data : {
+        user: userResponse,
+        accessToken,
+        refreshToken
+      }
+    });
+
+  } catch (error) {
+    next(error)
+  }
+};
+
