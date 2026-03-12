8afd8b01c134cf535bfdadeb7570042e6c36eba0
Naeem <135511325+Rehman32@users.noreply.github.com>
2026-03-13 02:13:01 +0500

    write setup.py to use the ml_core in ml_service

---
+++ ml_core/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="oncovisionx_core",
+    version="3.0.0",
+    packages=find_packages(where="."),
+    description="Core ML engine for OncoVisionX",
+)
